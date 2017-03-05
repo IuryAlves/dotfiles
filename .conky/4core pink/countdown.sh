88204e2cb92df5815b85f6c3c4404228400113fd
iury <satriani-16@hotmail.com>
2017-03-05 20:56:08 -0300

    removing conky files

--- .conky/4core pink/countdown.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-### Change the date using yyyy-mm-dd to the day you want to count down to ###
-echo $((($(date +%s -d "2013-06-22") - $(date +%s -d now ))/86400))
--- .conky/conky-startup.sh
+++
@@ -1,4 +0,0 @@
-sleep 0s
-killall conky
-cd "/home/iury/.conky/Conky Dark 4 cores"
-conky -c "/home/iury/.conky/Conky Dark 4 cores/conkyrc" &
