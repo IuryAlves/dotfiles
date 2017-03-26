e3aaa3f88b6beb990270f57988cf21c149a1d176
IuryAlves <satriani-16@hotmail.com>
2017-03-26 17:18:27 -0300

    add space-vim

--- README.md
+++ README.md
@@ -11,5 +11,13 @@ Just do :
 ./install.sh
 ```
 
+If you want to use space vim, just do:
+
+```sh
+
+./install.sh --use-space-vim
+
+```
+
 
 License [MIT](LICENSE)
--- install.sh
+++ install.sh
@@ -2,27 +2,40 @@
 
 current_dir="$(pwd)"
 
-echo "Creating symbolic link to .vimrc"
 
-ln -s $current_dir/.vimrc ~/.vimrc
+install_space_vim (){ 
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/liuchengxu/space-vim/master/install.sh)"
+}
 
-echo "Creating symbolic link to .vim"
 
-ln -s $current_dir/.vim ~/.vim
+if [ "$1" = "--use-space-vim" ]; then
+    echo "Installing space vim... "
+    install_space_vim
+else
+    echo "Creating symbolic link to .vimrc"
+
+    ln -snf "$current_dir/.vimrc" ~/.vimrc
+
+    echo "Creating symbolic link to .vim"
+
+    ln -snf $current_dir/.vim ~/.vim
+
+fi
+
 
 echo "Creating symbolic link to .zshrc"
 
-ln -s $current_dir/.zshrc ~/.zshrc
+ln -snf $current_dir/.zshrc ~/.zshrc
 
 echo "Creating symbolic link to .bashrc"
 
-ln -s $current_dir/.bashrc ~/.bashrc
+ln -snf $current_dir/.bashrc ~/.bashrc
 
 echo "Creating symbolic link to .editorconfig"
 
-ln -s $current_dir/.editorconfig ~/.editorconfig
+ln -snf $current_dir/.editorconfig ~/.editorconfig
 
 echo "Creating symbolic link to .git_functions.sh"
 
-ln -s $current_dir/.git_functions.sh ~/.git_functions.sh
+ln -snf $current_dir/.git_functions.sh ~/.git_functions.sh
 
